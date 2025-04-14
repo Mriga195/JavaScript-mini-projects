bb0210aaaba5e4115bedd23a05675f1f97a34455
Mriga195 <mrigank195@gmail.com>
2025-04-14 18:57:34 +0530

    classes done:)

--- classes/script.js
+++ classes/script.js
@@ -6,14 +6,18 @@ class Product {
 
     displayProduct(){
         console.log(`Product: ${this.name}`);
-        console.log(`Price: ${this.price}`);
+        console.log(`Price: ${this.price.toFixed(2)}`);
     }
 
     calculateTOtal(salesTax){
-        return this.price + (this.price * (salesTax / 100.0));
+        return (this.price + (this.price * (salesTax / 100.0))).toFixed(2);
     }
 }
 
 const product1 =  new Product("Shirt", 19.99);
+const product2 =  new Product("Laptop", 1200.99);
+const product3 =  new Product("aeroplane", 600000.99);
 
 product1.displayProduct();
+console.log(product2.calculateTOtal(7));
+product3.displayProduct();
